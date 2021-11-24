522d0e0bbab9e9231c75777ece8f833c30f44a11
KangHayeonn <rkdgkdus522@naver.com>
2021-11-24 17:23:16 +0900

    스프링 DB 접근 기술 - JPA
    * JPA 간단 구현
    
    - 작성자 : 강하연
    - 작성일 : 2021-11-24

--- study_IntroductionSpring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ study_IntroductionSpring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,33 @@
 package hello.hellospring;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.hibernate.persister.walking.spi.EntityIdentifierDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    /*
     private final DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    }*/
+
+    // @PersistenceContext
+    private final EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -31,6 +39,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
