02f9c0326991eadaa2a703c289c00bb4f7443d08
KangHayeonn <rkdgkdus522@naver.com>
2021-12-01 12:07:46 +0900

    EVENT 생성 API 구현 : 201 응답 받기
    * EventController & EventControllerTests 작성
    * (POST 동작 테스트) ID 설정, 201 응답 나옴, Test 성공
    
    - 작성자 : 강하연
    - 작성일 : 2021-12-01

--- study/rest-api-with-spring/src/test/java/me/whiteship/restapiwithspring/events/EventControllerTests.java
+++ study/rest-api-with-spring/src/test/java/me/whiteship/restapiwithspring/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package me.whiteship.restapiwithspring.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,12 +24,30 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Deveplopment with Spring")
+                .beginEnrollmentDataTime(LocalDateTime.of(2021, 11, 29, 15,55))
+                .closeEnrollmentDataTime(LocalDateTime.of(2021, 11, 30, 15,55))
+                .beginEventDateTime(LocalDateTime.of(2021, 12, 01, 11, 00))
+                .endEventDateTime(LocalDateTime.of(2021, 12, 02, 11, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
         mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaTypes.HAL_JSON)
-                        )
-                .andExpect(status().isCreated());
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
