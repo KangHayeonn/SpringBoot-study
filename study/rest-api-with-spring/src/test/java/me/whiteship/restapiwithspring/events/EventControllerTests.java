018bc02285cb8ba4939aa17aadd133417a5b5525
KangHayeonn <rkdgkdus522@naver.com>
2021-12-01 15:08:29 +0900

    fail on unknown properties

--- study/rest-api-with-spring/src/test/java/me/whiteship/restapiwithspring/events/EventControllerTests.java
+++ study/rest-api-with-spring/src/test/java/me/whiteship/restapiwithspring/events/EventControllerTests.java
@@ -35,7 +35,37 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Deveplopment with Spring")
+                .beginEnrollmentDataTime(LocalDateTime.of(2021, 11, 29, 15,55))
+                .closeEnrollmentDataTime(LocalDateTime.of(2021, 11, 30, 15,55))
+                .beginEventDateTime(LocalDateTime.of(2021, 12, 01, 11, 00))
+                .endEventDateTime(LocalDateTime.of(2021, 12, 02, 11, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
+                .id(100)
                 .name("Spring")
                 .description("REST API Deveplopment with Spring")
                 .beginEnrollmentDataTime(LocalDateTime.of(2021, 11, 29, 15,55))
@@ -56,12 +86,6 @@ public class EventControllerTests {
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest());
     }
 }
